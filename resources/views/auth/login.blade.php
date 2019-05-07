a41622e65ec71a9ed25f4e8b91e9592261f870cd
MohammadJavad <mj.ranjbar.94@gmail.com>
2019-05-07 11:59:07 -0400

    Group Route for Auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,22 @@
 <html>
 <body>
-<form method="post" action="login">
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+<form method="post" action="/login">
 
-    <input type="email" name="email" placeholder="email" size="40"><br><br>
+    <input type="text" name="email" placeholder="email" size="40"><br><br>
     <input type="password" name="password" placeholder="password" size="40"><br><br>
     <input hidden name="_token" value="{{csrf_token()}}">
     <input type="submit" value="send">
 </form>
+
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,24 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-
 Route::group(['middleware'=>'auth'],function (){
 
-    Route::get('', function () {
-
+    Route::get('/', function () {
         session(['a'=>'webshop']);
         //echo session('a');
         return view('welcome',['a'=>session('a')]);
     });
-    Route::get('home',function(){
-
+    Route::get('/home',function(){
         return view('welcome');
-
     });
+
 
 });
 
 
 
-
-//Create Auth
-
-Route::get('login',function (){
+Route::get('/login',function (){
     return view('auth.login');
-})->name('login');
+});
+
 Route::post('login','Auth\LoginController@login');
 
 Route::get('logout','Auth\LoginController@logout');
